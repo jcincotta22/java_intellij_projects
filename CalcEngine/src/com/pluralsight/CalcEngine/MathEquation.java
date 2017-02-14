5d5e485670c0138f270c5b5a6af630b42ed07dd9
jcincotta22 <jcincotta22@gmail.com>
2017-02-14 11:17:38 -0500

    overloading

--- CalcEngine/src/com/pluralsight/CalcEngine/MathEquation.java
+++ CalcEngine/src/com/pluralsight/CalcEngine/MathEquation.java
@@ -6,7 +6,7 @@ package com.pluralsight.CalcEngine;
 public class MathEquation {
     private double leftVal;
     private double rightVal;
-    private char opCode;
+    private char opCode = 'a';
     private double result;
 
     public double getLeftVal() {return leftVal;}
@@ -15,7 +15,35 @@ public class MathEquation {
     public void setRightVal(double rightVal) {this.rightVal = rightVal;}
     public char getopCode() {return opCode;}
     public void setOpCode(char opCode) {this.opCode = opCode;}
+
     public double getResult() {return result;}
+
+    public MathEquation() {}
+    public MathEquation (char opCode) {
+        this.opCode = opCode;
+    }
+
+    public MathEquation(char opCode, double leftVal, double rightVal) {
+        this(opCode);
+        this.leftVal = leftVal;
+        this.rightVal = rightVal;
+    }
+
+    public void execute(double leftVal, double rightVal) {
+        this.leftVal = leftVal;
+        this.rightVal = rightVal;
+
+        execute();
+    }
+
+    public void execute(int leftVal, int rightVal) {
+        this.leftVal = leftVal;
+        this.rightVal = rightVal;
+
+        execute();
+
+        result = (int)result;
+    }
 
     public void execute(){
         switch(opCode){
